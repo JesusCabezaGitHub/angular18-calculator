8c218b2f5f577b91197daa2b30b274f55d1a91ac
Jesus Cabeza <exjecaa@bci.cl>
2024-09-02 08:53:13 -0700

    first changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,39 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular-app-v18';
+  private count = signal(0)
+
+  ngOnInit() {
+    // let count = 0
+    // const doubleCount: Signal<number> = computed(()=> this.count() * 2)
+    // while (count < 10){
+    //   this.count.update( value => value + 1)
+    //   console.log('The count is: ' + this.count())
+    //   console.log('Double count:' + doubleCount())
+    //   count++
+    // }
+    // this.count.set(150)
+    // console.log(this.count())
+    // console.log('Double count:' + doubleCount())
+
+    const showCount = signal(false)
+    const otherCount = signal(0)
+    const conditionalCount = computed(()=> {
+      if(showCount()){
+        return `The count is ${otherCount()}`
+      }else {
+        return `Nothing to see here`
+      }
+    })
+    console.log(conditionalCount())
+    otherCount.set(15)
+    console.log(conditionalCount())
+    showCount.set(true)
+    console.log(conditionalCount())
+
+    
+
+    
+  }
+
 }
